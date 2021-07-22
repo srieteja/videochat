e678ddd3adf1bca0c630ab335502226fcd0eca9e
Srie Teja <srit1505@gmail.com>
2021-07-22 14:05:38 +0530

    final

--- src/Context.js
+++ src/Context.js
@@ -40,7 +40,7 @@ const ContextProvider = ({ children }) => {
   const answerCall = () => {
     setCallAccepted(true);
 
-    const peer = new Peer({ initiator: true, trickle: true, stream });
+    const peer = new Peer({ initiator: false, trickle: true, stream });
 
     peer.on('signal', (data) => {
       socket.emit('answerCall', { signal: data, to: call.from });
